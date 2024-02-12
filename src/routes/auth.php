37c45e8cf955556c9ee1f426596bb75be8c280db
amoschou <4948673+amoschou@users.noreply.github.com>
2024-02-13 00:09:00 +1030

    Updates

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -1,15 +1,18 @@
 <?php
 
-use App\Http\Controllers\LoginController;
+use HamiltonSC\Auth\App\Http\Controllers\GoogleLoginController;
+use AMoschou\RemoteAuth\App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Auth;
+
+$LoginController__class = config('remote_auth.socialite.google', false) ? GoogleLoginController::class : LoginController::class;
 
 Route::middleware('guest')->group(function () {
     Route::get('/auth/login', function () { return view('vendor.laravel-remote-auth.login'); })->name('login');
-    Route::post('/auth/login', [LoginController::class, 'authenticate'])->name('login.post');
+    Route::post('/auth/login', [$LoginController__class, 'authenticate'])->name('login.post');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/auth/callback', [LoginController::class, 'callback'])->name('google.callback');
+    Route::get('/auth/callback', [$LoginController__class, 'callback'])->name('google.callback');
 
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
     Route::get('/home', function () { return view('home'); })->name('home');
